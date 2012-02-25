fda67d32ace261a94c75dfd0dc7514d7303530c8
Benjamin Klüglein <scheibenkaes@googlemail.com>
2012-02-25 20:53:39 +0100

    v1.0.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clean-desk "1.0.0-SNAPSHOT"
+(defproject clean-desk "1.0.0"
   :description "Now get up and clean your desk"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/clojure-contrib "1.2.0"]])
